53b05cf72ff8f5adeffc8370b42eead967d9112b
Cristyan Manta Caro <cristyan.manta@gmail.com>
2023-04-23 12:30:01 +0200

    Add files via upload

---
+++ sim_controls.py
@@ -0,0 +1,10 @@
+# In[]:
+# Controls for WIM.WoT webapp
+
+ENTITY_TYPES = dict(
+    IZN="Intelligent Zones",
+    SSP="Smart Spaces",
+    SSS="Smart Sub Spaces",
+    VTH="Virtual Things",
+    VSN="Virtual Sensors"
+)
